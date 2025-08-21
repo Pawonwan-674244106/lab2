4c98a73a8a28466068d3acc02cb6cec34144ed3d
Pawonwan-674244106 <674244106@mail.pbru.ac.th>
2025-08-21 11:47:51 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+ STARTUPINFO si;
+ PROCESS_INFORMATION pi;
+ ZeroMemory(&si, sizeof(si));
+ si.cb = sizeof(si);
+ ZeroMemory(&pi, sizeof(pi));
+ printf("Parent Process ID: %lu\n", GetCurrentProcessId());
+ printf("Parent Thread ID: %lu\n", GetCurrentThreadId());
+ CloseHandle(pi.hProcess);
+ CloseHandle(pi.hThread);
+ return 0;
+}
+
+
